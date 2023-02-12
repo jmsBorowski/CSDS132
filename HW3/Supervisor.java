652d2878fb93fa309482e372db5dfe3ab6f8cb1a
Jaiden Borowski <78442041+jmsBorowski@users.noreply.github.com>
2023-02-11 20:35:07 -0500

    Create Supervisor.java

---
+++ HW3/Supervisor.java
@@ -0,0 +1,8 @@
+/** 
+ * Jaiden Borowski 
+ * CSDS 132 
+ * Supervisor class - The Supervisor class is extended by other classes to 
+distinguish them as Supervisors 
+ */ 
+public interface Supervisor {
+} 
